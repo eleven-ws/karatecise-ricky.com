5867a0e4809cd41a87a9d6053756c8c724a38a95
Kojiro Ozaki <ozaki@eleven-ws.com>
2024-03-10 00:12:28 +0900

    Updates

---
+++ component---src-pages-404-js-9b9503a9979eacc06db9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkaratecise_ricky_com=self.webpackChunkkaratecise_ricky_com||[]).push([[883],{429:function(t,e,a){a.r(e);a(2784);var n=a(8671),c=a(917);e.default=t=>{let{data:{site:{siteMetadata:{siteUrl:e}}}}=t;return(0,c.tZ)(n.q,null,(0,c.tZ)("meta",{name:"robots",content:"noindex"}),(0,c.tZ)("meta",{"http-equiv":"Refresh",content:`0; URL=${e}`}))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9b9503a9979eacc06db9.js.map
